13ccf8b23dc8120a010de3c3f2b6fde615ed8fb6
Gita Permatasari Sujatmiko <gita.permatasari91@ui.ac.id>
2022-06-08 09:09:33 +0700

    Implement message API

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+
+// Configuring body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.end("Welcome to Siloka Backend");
+});
+
+app.post("/message", (req, res) => {
+  res.send(req.body);
+});
+
+app.post("/message-shortcut", (req, res) => {
+  // TODO
+});
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
