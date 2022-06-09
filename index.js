47be7eabb55fc7d0e6e830fa5a76e5cec18c1309
Gita Permatasari Sujatmiko <gita.permatasari91@ui.ac.id>
2022-06-09 14:03:10 +0700

    Call ml gateway for messages

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+var http = require("http");
 
 const app = express();
 const port = 3000;
@@ -12,15 +13,30 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
-  res.send("Welcome to Siloka Backend");
+  res.end("Welcome to Siloka Backend");
 });
 
 app.get("/to-cs", (req, res) => {
-  res.send("Directing to customer service...");
+  res.end("Directing to customer service...");
 });
 
 app.post("/message", (req, res) => {
-  res.send(req.body);
+  var payload = { messages: req.body };
+  var request = http.request(
+    {
+      host: "34.87.1.81",
+      port: 80,
+      path: "/predict/",
+      method: "POST",
+      body: JSON.stringify(payload),
+    },
+    function (response) {
+      response.on("data", function (chunk) {
+        res.send(chunk);
+      });
+    }
+  );
+  request.end();
 });
 
 app.post("/message-shortcut", (req, res) => {
