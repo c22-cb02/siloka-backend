4f5caa0131663eb6f22d57a6a853227c4a0d5f60
Gita Permatasari Sujatmiko <gita.permatasari91@ui.ac.id>
2022-06-10 09:12:32 +0700

    Implement add ToCS, SuccessRate, and Message entities to Datastore

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import express from "express";
 import cors from "cors";
 import axios from "axios";
+import { Datastore } from "@google-cloud/datastore";
 
 const app = express();
+const datastore = new Datastore();
 const port = process.env.NODE_ENV === "production" ? 80 : 3000;
 
 // Configuring body parser middleware
@@ -34,9 +36,87 @@ app.post("/message", async (req, res) => {
   res.json({ message: predictionData.predicted_response });
 });
 
-app.post("/message-shortcut", (req, res) => {
-  // TODO
-});
+async function addToCS(room_id, choice) {
+  const toCSKey = datastore.key("ToCS");
+  const entity = {
+    key: toCSKey,
+    data: [
+      {
+        name: "room_id",
+        value: room_id,
+      },
+      {
+        name: "is_to_cs",
+        value: choice,
+      },
+    ],
+  };
+
+  try {
+    await datastore.save(entity);
+    console.log(`ToCS ${toCSKey.id} created successfully.`);
+  } catch (err) {
+    console.error("ERROR:", err);
+  }
+}
+
+async function addSuccessRate(room_id, request, response, feedback) {
+  const successRateKey = datastore.key("SuccessRate");
+  const entity = {
+    key: successRateKey,
+    data: [
+      {
+        name: "room_id",
+        value: room_id,
+      },
+      {
+        name: "request",
+        value: request,
+        excludeFromIndexes: true,
+      },
+      {
+        name: "response",
+        value: response,
+        excludeFromIndexes: true,
+      },
+      {
+        name: "feedback",
+        value: feedback,
+      },
+    ],
+  };
+
+  try {
+    await datastore.save(entity);
+    console.log(`SuccessRate ${successRateKey.id} created successfully.`);
+  } catch (err) {
+    console.error("ERROR:", err);
+  }
+}
+
+async function addMessage(room_id, message) {
+  const messageKey = datastore.key("Message");
+  const entity = {
+    key: messageKey,
+    data: [
+      {
+        name: "room_id",
+        value: room_id,
+      },
+      {
+        name: "message",
+        value: message,
+      },
+    ],
+  };
+
+  try {
+    await datastore.save(entity);
+    console.log(`Message ${messageKey.id} created successfully.`);
+  } catch (err) {
+    console.error("ERROR:", err);
+  }
+}
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
