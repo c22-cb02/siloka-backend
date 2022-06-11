53086f4d69f24cf2188f93a8dbb78da78aea6375
Dennis A. Walangadi <dennisaw2000@gmail.com>
2022-06-11 14:35:54 +0700

    refactor: simplify microbenchmark decorator

--- microbenchmark.js
+++ microbenchmark.js
@@ -4,11 +4,10 @@ const getNS = (res) => res[0] * 1e9 + res[1];
 
 export default function microbenchmark(wrapped) {
   return function decorator(...args) {
-    const original = wrapped;
-    if (typeof original === 'function') {
+    if (typeof wrapped === 'function') {
       try {
         const start = getNS(process.hrtime());
-        const result = original.apply(this, args);
+        const result = wrapped.apply(this, args);
         const end = getNS(process.hrtime());
         const delta = end - start
         logger.info(`Function ${wrapped.name} ran in ${delta * 1000} nanoseconds`);
